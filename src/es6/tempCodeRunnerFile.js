524c1d865862785cbaafef36ff588c65d5bbe43d
Heimer Martinez <heimer79@iMac-de-Heimer.local>
2020-03-15 11:05:19 -0500

    guardando es7 t es6

--- src/es6/tempCodeRunnerFile.js
+++ src/es6/tempCodeRunnerFile.js
@@ -1,1 +1,13 @@
-hello();
+function* helloWorld() {
+    if (true) {
+        yield ' Hello,  ';
+    }
+    if (true) {
+        yield 'World';
+    }
+}
+
+const generatorHello = helloWorld();
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
