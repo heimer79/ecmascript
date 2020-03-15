422a2006f2549f5f24f78e6329aed29fa9695412
Heimer Martinez <heimer79@iMac-de-Heimer.local>
2020-03-14 23:37:56 -0500

    objetos  , promesas, clases y modulos

--- src/es6/index.js
+++ src/es6/index.js
@@ -121,3 +121,27 @@ helloPromise()
     .catch(error => console.log(error))
 
 
+class calculator {
+    constructor() {
+        this.valueA = 0;
+        this.valueB = 0;
+
+    }
+
+    sum(valueA, valueB) {
+        this.valueA = valueA;
+        this.valueB = valueB;
+
+        return this.valueA + this.valueB;
+    }
+}
+
+const calc = new calculator();
+
+console.log(calc.sum(2, 2));
+
+import { hello } from './module';
+
+hello();
+
+
--- src/es6/tempCodeRunnerFile.js
+++ src/es6/tempCodeRunnerFile.js
@@ -1,8 +1,1 @@
-let name = 'heimer';
-let age = 40;
-
-obj = { name: name, age: age };
-
-obj2 = { name, age };
-
-console.log(obj2);
+hello();
