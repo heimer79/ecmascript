cc028314714fef73fa5b4718e22edd801e6ccdc7
Heimer Martinez <heimer79@iMac-de-Heimer.local>
2020-03-15 11:02:31 -0500

    generadores usando yield para marcar los pasos del algoritmo y mostrando con next

--- src/es6/index.js
+++ src/es6/index.js
@@ -144,4 +144,17 @@ import { hello } from './module';
 
 hello();
 
+//generators
+function* helloWorld() {
+    if (true) {
+        yield ' Hello,  ';
+    }
+    if (true) {
+        yield 'World';
+    }
+}
 
+const generatorHello = helloWorld();
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
