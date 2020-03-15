2e2988589fec56b43a1018b9f0936979baab7ef7
Heimer Martinez <heimer79@iMac-de-Heimer.local>
2020-03-15 11:04:54 -0500

    aplicaciones de ecmascript 7 con include y potencia

---
+++ src/es7/index.js
@@ -0,0 +1,20 @@
+// valor include en vez de index of
+
+let numbers = [1, 2, 3, 7, 8];
+
+if (numbers.includes(7)) {
+    console.log('Si se encuentra el valor 7')
+} else {
+    console.log('No se encuentra')
+}
+
+
+// elevar a la potencia antes con pow Math.pow(4, 3);
+
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+
+console.log(result);
+
+
