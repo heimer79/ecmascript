274a844f905cfbf4c60f69fe376ddf1f9bf29e02
Heimer Martinez <heimer79@iMac-de-Heimer.local>
2020-03-17 08:59:21 -0500

    uso de async, await y try catch

--- src/es8/index.js
+++ src/es8/index.js
@@ -34,3 +34,33 @@ console.log('food'.padEnd(12, '   -----'));
 const obj = {
     name: 'oscar',
 }
+
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (false)
+            ? setTimeout(() => resolve('Hello World'), 3000)
+            : reject(new Error('Test Error'))
+    })
+}
+
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    //const hello2 = await
+    console.log(hello);
+}
+
+helloAsync();
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+anotherFunction();
+
+
+
